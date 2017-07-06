4277d02c28a68ee99fb5f18f2c1abf6ad63cf2c7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-06 12:24:15 -0400

    initial

---
+++ src/prefixed-list-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const prefixedList = require('.')
+
+describe('prefixed-list', () => {
+  it('write this test', () => {
+    console.assert(prefixedList, 'should export something')
+  })
+})
